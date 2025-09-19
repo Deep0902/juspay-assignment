b66606d97d2a64411eeedaef572e1788449f0509
Deep Rakesh <deeptank09@gmail.com>
2025-09-19 12:57:51 +0530

    Right panel UI

--- src/components/RightPanel/Activities/Activities.jsx
+++ src/components/RightPanel/Activities/Activities.jsx
@@ -6,25 +6,28 @@ function Activities() {
   return (
     <div className="activities-panel">
       <h2 className="activities-title">Activities</h2>
-      <div className="activity-list">
-        {activities.map((act, idx) => (
-          <div
-            className={`notification-item notification-${act.type}`}
-            key={idx}
-          >
-            <img
-              src={`/avatar/Avatar${act.user}.svg`}
-              alt={act.type}
-              className="notification-icon"
-            />
-            <div className="notification-content">
-              <div className="notification-heading">
-                {truncateText(act.heading)}
+      <div className="activity-list-wrapper" style={{ position: "relative" }}>
+        <div className="activity-vertical-line" />
+        <div className="activity-list">
+          {activities.map((act, idx) => (
+            <div
+              className={`activities-item notification-${act.type}`}
+              key={idx}
+            >
+              <img
+                src={`/avatar/Avatar${act.user}.svg`}
+                alt={act.type}
+                className="notification-icon"
+              />
+              <div className="notification-content">
+                <div className="notification-heading">
+                  {truncateText(act.heading)}
+                </div>
+                <div className="notification-time">{act.time}</div>
               </div>
-              <div className="notification-time">{act.time}</div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/RightPanel/Contacts/Contacts.jsx
+++ src/components/RightPanel/Contacts/Contacts.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import contacts from "../../../data/contacts";
+import { truncateText } from "../../../util/commonFunctions";
 import "./Contacts.css";
-
 function Contacts() {
   return (
     <div className="contacts-panel">
       <h2 className="contacts-title">Contacts</h2>
-      {/* Add contact items here */}
+      <div className="notification-list">
+        {contacts.map((notif, idx) => (
+          <div
+            className={`contact-item notification-${notif.type}`}
+            key={idx}
+          >
+            <img
+              src={`/contacts/Contact${notif.user}.svg`}
+              alt={notif.type}
+              className="notification-icon"
+            />
+            <div className="notification-content">
+              <div className="notification-heading">
+                {truncateText(notif.name)}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
