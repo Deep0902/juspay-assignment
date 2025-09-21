6480c961b20a53743f66bc2f4c2fc846448b68e0
Deep Rakesh <deeptank09@gmail.com>
2025-09-21 14:32:01 +0530

    icons added for PWA

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: 'auto',
+      injectRegister: "auto",
 
       pwaAssets: {
         disabled: false,
@@ -20,6 +19,74 @@ export default defineConfig({
         short_name: "juspay-assignment",
         description: "A project made by Deep for Juspay Assignment",
         theme_color: "#ffffff",
+        icons: [
+          {
+            src: "bug.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "ClockCounterClockwise.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "favicon.png",
+            sizes: "any",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "juspaylogo.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "new_user.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "Profile.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "Sidebar.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "Star.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "subscription.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "Sun.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+        ],
       },
 
       workbox: {
