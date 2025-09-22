1f3eef26a982b9fb8ccf8f8915e8fab3e952cf2e
Deep Rakesh <deeptank09@gmail.com>
2025-09-22 13:46:03 +0530

    added data

--- src/data/leftPanel.js
+++ src/data/leftPanel.js
@@ -4,21 +4,37 @@ export const dashboardMenuItems = [
     id: "default",
     label: "Default",
     type: "item",
+    children: [
+      { id: "submenu1", label: "Sub Menu 1" },
+      { id: "submenu2", label: "Sub Menu 2" },
+    ],
   },
   {
     id: "ecommerce",
     label: "eCommerce",
     type: "expandable",
+    children: [
+      { id: "submenu1", label: "Sub Menu 1" },
+      { id: "submenu2", label: "Sub Menu 2" },
+    ],
   },
   {
     id: "projects",
     label: "Projects",
     type: "expandable",
+    children: [
+      { id: "submenu1", label: "Sub Menu 1" },
+      { id: "submenu2", label: "Sub Menu 2" },
+    ],
   },
   {
     id: "onlineCourses",
     label: "Online Courses",
     type: "expandable",
+    children: [
+      { id: "submenu1", label: "Sub Menu 1" },
+      { id: "submenu2", label: "Sub Menu 2" },
+    ],
   },
 ];
 
@@ -40,20 +56,36 @@ export const pagesMenuItems = [
     id: "account",
     label: "Account",
     type: "expandable",
+    children: [
+      { id: "submenu1", label: "Sub Menu 1" },
+      { id: "submenu2", label: "Sub Menu 2" },
+    ],
   },
   {
     id: "corporate",
     label: "Corporate",
     type: "expandable",
+    children: [
+      { id: "submenu1", label: "Sub Menu 1" },
+      { id: "submenu2", label: "Sub Menu 2" },
+    ],
   },
   {
     id: "blog",
     label: "Blog",
     type: "expandable",
+    children: [
+      { id: "submenu1", label: "Sub Menu 1" },
+      { id: "submenu2", label: "Sub Menu 2" },
+    ],
   },
   {
     id: "social",
     label: "Social",
     type: "expandable",
+    children: [
+      { id: "submenu1", label: "Sub Menu 1" },
+      { id: "submenu2", label: "Sub Menu 2" },
+    ],
   },
 ];
